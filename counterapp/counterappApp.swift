8861f627ea986100b61ac5c9b039bb599b95224b
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-14 08:20:26 +0900

    Initial Commit

---
+++ counterapp/counterappApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterappApp.swift
+//  counterapp
+//
+//  Created by evesquare on 2025/04/14.
+//
+
+import SwiftUI
+
+@main
+struct counterappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ counterappTests/counterappTests.swift
@@ -0,0 +1,17 @@
+//
+//  counterappTests.swift
+//  counterappTests
+//
+//  Created by evesquare on 2025/04/14.
+//
+
+import Testing
+@testable import counterapp
+
+struct counterappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
